9b924984713f8e4dc42ee3aa2ee5aeff2d08e490
Michael-Tinker <michaelt19@protonmail.com>
2022-04-11 15:44:54 -0500

    end of class 4.11

---
+++ excel1.py
@@ -0,0 +1,41 @@
+from tkinter import font
+import openpyxl as xl
+from openpyxl.styles import Font
+
+
+#create a new excel document
+wb = xl.Workbook()
+
+MySheet = wb.active
+
+MySheet.title = "First Sheet"
+
+#create a new worksheet
+wb.create_sheet(index=1,title="second sheet")
+
+#write content to a cell
+MySheet['A1']='An example of Sum Formula'
+
+#change the font size and italicize
+MySheet['A1'].font = Font(name="Times New Roman", size=24, italic=True,bold=True)
+
+#alternatively you can create a font object and assign it
+
+fontobject = Font(name="Times New Roman", size=24, italic=True,bold=True)
+MySheet['A1'].font = fontobject
+
+#adding values to cells
+MySheet['B2'] = 50
+MySheet['B3'] = 75
+MySheet['B4'] = 100
+MySheet['A5'] = "Total"
+MySheet['A5'].font = Font(size=16,bold=True)
+
+MySheet['B5'] = '=SUM(B2:B4)'
+
+#change the column width
+MySheet.column_dimensions['A'].width = 25
+
+
+
+wb.save("PythonToExcel.xlsx")
--- webscraping-movies.py
+++ webscraping-movies.py
@@ -1,4 +1,8 @@
 
+from tkinter import font
+import openpyxl as xl
+from openpyxl.styles import Font
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import openpyxl as xl
@@ -23,3 +27,73 @@ print(title.text)
 ##
 ##
 
+#create a new excel document
+wb = xl.Workbook()
+
+MySheet = wb.active
+
+MySheet.title = "First Sheet"
+
+
+#alternatively you can create a font object and assign it
+
+fontobject = Font(size=16, bold=True)
+MySheet['A1'].font = fontobject
+MySheet['B1'].font = fontobject
+MySheet['C1'].font = fontobject
+MySheet['D1'].font = fontobject
+MySheet['E1'].font = fontobject
+MySheet['F1'].font = fontobject
+
+
+table_rows = soup.findAll("tr")
+
+
+MySheet['A1'] = "Rank"
+MySheet['B1'] = "Title"
+MySheet['C1'] = "Release Date"
+MySheet['D1'] = "Gross"
+MySheet['E1'] = "Total Gross"
+MySheet['F1'] = "% of Total Gross"
+
+
+MySheet['C1'].font = fontobject
+MySheet['D1'].font = fontobject
+MySheet['E1'].font = fontobject
+MySheet['F1'].font = fontobject
+
+rownum = 2
+for row in table_rows[1:6]:
+    #print(row)
+    td = row.findAll("td")
+    rank = td[0].text.strip()
+    release = td[1].text.strip()
+    genre = td[2].text
+    money_type = td[3].text
+    duration = td[4].text
+    gross = td[5].text
+    gross_num = int(gross.replace(",","").replace("$",""))
+    theaters = td[6].text
+    total_gross = td[7].text
+    total_gross_num = int(total_gross.replace(",","").replace("$",""))
+    release_date = td[8].text
+    distributor = td[9].text
+
+    #print(rank + " - " + release + " - Gross Total: " + total_gross + "\n")
+
+    MySheet["A" + str(rownum)] = rank
+    MySheet["B" + str(rownum)] = release
+    MySheet["C" + str(rownum)] = release_date
+    MySheet["D" + str(rownum)] = gross
+    MySheet["E" + str(rownum)] = total_gross
+    MySheet["F" + str(rownum)] = str(round((gross_num/total_gross_num)*100,2)) + "%"
+
+    rownum += 1
+
+ws.column_dimension["A"].width = 5
+#30, 25, 16, 20 ,26
+
+#for cell in ws[1:1]:
+#    cell.font = header_font
+
+wb.save("PythonToExcel.xlsx")
